829358f9b87dab881f9f85e44c8da45d45026c2d
Michal Osinski <mosinski@hotmail.co.uk>
2017-05-14 15:41:29 +0000

    added hirb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514133637) do
+ActiveRecord::Schema.define(version: 20170514150728) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
